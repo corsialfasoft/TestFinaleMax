b09be7cbaf72168c87376ddd9541673d93e7d4ad
emmeacs <massimo.franzoso8895@gmail.com>
2018-05-09 14:25:31 +0200

    Esercizio 1 completo

--- TestMax1/Program.cs
+++ TestMax1/Program.cs
@@ -10,6 +10,30 @@ namespace TestMax1
 	{
 		static void Main(string[] args)
 		{
+
+			int k = StampaPari();
+			StampaDispari(k-1);
+		}
+		public static int StampaPari()
+		{
+			int i = 0;
+			for(int k = 0; k < 15; k++) {
+				Console.WriteLine($"... Numero pari: {i}");
+				if(k == 14) {
+					return i;
+				}
+				i += 2;
+			}
+			return i;
+		}
+		public static void StampaDispari(int d)
+		{
+			while(d >= 1) {
+				Console.WriteLine($"...Numero dispari:{d}");
+				d-=2;
+			}
 		}
 	}
 }
+
+
