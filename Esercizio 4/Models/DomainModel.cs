478b5c45777a1e01df576163d82342a3a55f7dfe
emmeacs <massimo.franzoso8895@gmail.com>
2018-05-09 17:30:42 +0200

    Sono arrivato fin qua.

--- Esercizio 4/Models/DomainModel.cs
+++ Esercizio 4/Models/DomainModel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace Esercizio_4.Models
 {
 	public class DomainModel
 	{
-		Fasulla fasulla = new Fasulla() ;
+		Fasulla fasulla = new Fasulla();
 		public List<Menu> GetLista(string a)
 		{
 			return fasulla.GetLista(a);
@@ -19,19 +20,19 @@ namespace Esercizio_4.Models
 		}
 		public void Add(Menu menu)
 		{
-		 fasulla.Add(menu);
+			fasulla.Add(menu);
 		}
 	}
 	public class Menu
 	{
-		public int Id { get;set;}
-		public DateTime Data { get;set;}
-		public string Pasto { get;set;}
-		public string Giorno { get;set;}
-		public string Primo { get;set;}
-		public string Secondo { get;set;}
-		public string Contorno { get;set;}
-		public string Dolce { get; set;}
+		public int Id { get; set; }
+		public DateTime Data { get; set; }
+		public string Pasto { get; set; }
+		public string Giorno { get; set; }
+		public string Primo { get; set; }
+		public string Secondo { get; set; }
+		public string Contorno { get; set; }
+		public string Dolce { get; set; }
 
 	}
 	public class Fasulla
@@ -50,12 +51,34 @@ namespace Esercizio_4.Models
 
 		public Menu Search(int id)
 		{
-			return new Menu { Id=77,Data=DateTime.Now, Pasto="Pranzo",Giorno="Mercoledi", Primo="Orecchiette alle cime di nutria", Secondo="Stufato di nutria",Contorno="Patate fritte",Dolce="Nutria Glassata"};
+			Menu menu = new Menu();
+			SqlConnection con = new SqlConnection(GetConnection());
+			try {
+				con.Open();
+				SqlCommand command = new SqlCommand("Search",con);
+				command.CommandType=System.Data.CommandType.StoredProcedure;
+				command.Parameters.Add("@id",System.Data.SqlDbType.Int).Value=id;
+				SqlDataReader reader= command.ExecuteReader();
+				while(reader.Read()) {
+					menu.
+				}
+				return menu;
+			}catch(Exception x) { throw x; } finally {
+				con.Dispose();
+			}
+			//return new Menu { Id = 77,Data = DateTime.Now,Pasto = "Pranzo",Giorno = "Mercoledi",Primo = "Orecchiette alle cime di nutria",Secondo = "Stufato di nutria",Contorno = "Patate fritte",Dolce = "Nutria Glassata" };
 		}
 
 		public void Add(Menu menu)
 		{
-			
+
+		}
+		private string GetConnection()
+		{
+			SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+			builder.DataSource = @"(localdb)\MSSQLLocalDB",
+			builder.InitialCatalog = "DaCiccio";
+			return builder.ToString();
 		}
 	}
 }
