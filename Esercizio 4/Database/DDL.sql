e7b997550ff2f3027aad2245821656ea0da05200
emmeacs <massimo.franzoso8895@gmail.com>
2018-05-09 17:08:56 +0200

    Creato database

---
+++ Esercizio 4/Database/DDL.sql
@@ -0,0 +1,14 @@
+﻿Create Database DaCiccio
+go
+use DaCiccio
+go
+Create Table Menu(
+id int identity(1,1) primary key not null,
+datag date not null,
+pasto nvarchar(20) not null,
+Giorno nvarchar(20) not null,
+primo nvarchar(20) ,
+secondo nvarchar(20),
+contorno nvarchar(20),
+dolce nvarchar(20)
+)
