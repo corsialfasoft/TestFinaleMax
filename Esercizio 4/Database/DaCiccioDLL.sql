c702dd4359ced78058124300aa941407ed65a6c6
emmeacs <massimo.franzoso8895@gmail.com>
2018-05-09 17:14:00 +0200

    Creata dll per database

---
+++ Esercizio 4/Database/DaCiccioDLL.sql
@@ -0,0 +1,6 @@
+﻿use DaCiccio
+go
+Insert into Menu (datag,pasto,Giorno,primo,secondo,contorno,dolce) values ('09/05/2018','pranzo','Lunedi','Pastachiipipi','Cavalli','Patate','omissis')
+Insert into Menu (datag,pasto,Giorno,primo,secondo,contorno,dolce) values ('09/05/2018','cena','Lunedi','Lasagne','oche','insalata','omissis')
+Insert into Menu (datag,pasto,Giorno,primo,secondo,contorno,dolce) values ('09/05/2018','pranzo','Martedì','pizza','galline','zucchine','omissis')
+Insert into Menu (datag,pasto,Giorno,primo,secondo,contorno,dolce) values ('09/05/2018','cena','Martedì','aria','gatti','carote','omissis')
